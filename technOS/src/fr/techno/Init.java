a3870e9a563e86f9cb8675901ffb785104787ada
TechnoSpiritMC <127853301+TechnoSpiritMC@users.noreply.github.com>
2024-03-15 11:56:53 +0000

    Pending changes exported from your codespace

--- technOS/src/fr/techno/Init.java
+++ technOS/src/fr/techno/Init.java
@@ -1,14 +1,10 @@
 package fr.techno;
 
-import java.io.File;
 
 public class Init {
     public static void main(String[] args) {
 
-        System.out.println(Utils.colored(175, 255, 125, "\033[1mBonjour et bienvenue sur Xtentions\033[0m"));
-
-        System.out.println(Utils.colored(125, 255, 175, LoadConfig.getConfig().get("name")));
-        System.out.println(Utils.colored(125, 255, 175, LoadConfig.getConfig().get("version")));
+        System.out.println(Utils.colored(175, 255, 125, "\033[1mWelcome to TechnOS\033[0m"));
     }
 
 }
